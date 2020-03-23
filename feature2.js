2e820a8bc4950316eba9756cc9012232c13b8fb6
Danny Hoang <hnthai@humaxdigital.com>
2020-03-23 10:17:12 +0700

    on new_feature, update test.js, update feature2.js

--- feature2.js
+++ feature2.js
@@ -1,1 +1,2 @@
 console.log('update feature 2')
+console.log('update feateure 2 -1')
--- test.js
+++ test.js
@@ -1,3 +1,6 @@
 console.log('a1')
 
 console.log('update main');
+
+
+console.log('on new_feature, update test.js')
